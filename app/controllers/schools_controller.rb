48be78db4a961563f24b828a1e393720011cfa2f
Moose <Davis>
2021-05-25 08:45:03 -0500

    added json to index and show of schools and recipients

--- app/controllers/schools_controller.rb
+++ app/controllers/schools_controller.rb
@@ -4,10 +4,13 @@ class SchoolsController < ApplicationController
   # GET /schools or /schools.json
   def index
     @schools = School.all
+    render json: @schools
   end
 
   # GET /schools/1 or /schools/1.json
   def show
+    @school = School.find_by(id: params[:id])
+    render json: @school
   end
 
   # GET /schools/new
