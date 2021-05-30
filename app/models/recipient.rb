2e92295c5631ad202ef7158bcfa95dc4fa4a00a9
Moose <Davis>
2021-05-30 10:05:06 -0500

    added validations for name&address for School and Recipients

--- app/models/recipient.rb
+++ app/models/recipient.rb
@@ -1,3 +1,4 @@
 class Recipient < ApplicationRecord
     belongs_to :school
+    validates :name, :address, presence: true
 end
--- app/models/school.rb
+++ app/models/school.rb
@@ -1,3 +1,5 @@
 class School < ApplicationRecord
     has_many :recipients
+    validates :name, :address, presence: true 
+    validates :name, :address, unniqueness: true 
 end
